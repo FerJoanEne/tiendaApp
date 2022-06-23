f32ee66083aaffd69e34fba6a8de1ea4009f19ac
JoanF <fernandoene91@gmail.com>
2022-06-23 14:31:12 -0300

    pensando el CLI

--- tiendaApp/src/main/java/main/Main.java
+++ tiendaApp/src/main/java/main/Main.java
@@ -17,13 +17,15 @@ public class Main
 	public static void main(String[] args) 
 	{
 		
-		 Scanner teclado=new Scanner(System.in);
-	        String nombre1,apellido;
-	        System.out.print("Ingrese el nombre:");
-	        nombre1=teclado.next();
-	        System.out.print("Ingrese apellido:");
-	        apellido=teclado.next();        
-	        
-	        CLI.limpiarConsola();
+		 /*Scanner teclado=new Scanner(System.in);
+	     String nombre1,apellido;
+	     System.out.print("Ingrese el nombre:");
+	     nombre1=teclado.next();
+	     System.out.print("Ingrese apellido:");
+	     apellido=teclado.next();        
+	     */   
+	        //CLI.limpiarConsola();
+
+
     }
 }
--- tiendaApp/src/main/java/presentacion/CLI.java
+++ tiendaApp/src/main/java/presentacion/CLI.java
@@ -7,20 +7,31 @@ public class CLI {
 	public static void mostrarMenuPrincipal() {
 		int input = 0;
 		Scanner sc = new Scanner(System.in);
-		
 		while(input != 5) {
 			System.out.println("1. PRODUCTOS");
 			System.out.println("2. VENDEDORES");
 			System.out.println("3. VENTAS");
 			System.out.println("4. FINALIZAR");
-			
+			input = sc.nextInt();
+			comprobarOpcion(input);
 		}
 	}
 	
+	private static void comprobarOpcion(int input) {
+		validarInput(input);
+		
+	}
+
+	private static void validarInput(Object input) {
+		
+		
+		
+	}
+
 	public static void limpiarConsola(){
         try{
         	
-        	if (System.getProperty("os.name").contains("windows"))
+        	if (System.getProperty("os.name").contains("Windows"))
             {
               new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             }
