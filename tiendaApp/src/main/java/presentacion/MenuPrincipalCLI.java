84d15ed033552b03e11269377ce5fc8c17266014
JoanF <fernandoene91@gmail.com>
2022-06-28 18:25:49 -0300

    volviendo a implementar menu principal y funcionalidades

--- tiendaApp/src/main/java/presentacion/MenuPrincipalCLI.java
+++ tiendaApp/src/main/java/presentacion/MenuPrincipalCLI.java
@@ -4,59 +4,191 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import modelo.Producto;
+import modelo.Vendedor;
+import persistencia.transacciones.ProductoSQL;
+import persistencia.transacciones.VendedorSQL;
+
 
 public class MenuPrincipalCLI {
 	
-	public MenuPrincipalCLI() {
-		limpiarConsola();
-		int input = 0;
+	private String input;
+	
+	public void mostrarMenu() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("MENU PRINCIPAL");
-		System.out.println("1. PRODUCTOS");
-		System.out.println("2. VENDEDORES");
-		System.out.println("3. VENTAS");
-		System.out.println("4. FINALIZAR");
-		System.out.println("------------------------------------------------------------------");
-		input = sc.nextInt();
-		if(validarInput(input)) {
-			mostrarMenu(input);
-		} else {
-			new MenuPrincipalCLI();
-		}
-		
+		System.out.println("Elija una opcion [1 - 10]");
+		System.out.println("---------------------:");
+		System.out.println("1. Registrar producto");
+		System.out.println("2. Registrar vendedor");
+		System.out.println("3. Registrar venta");
+		System.out.println("---------------------");
+		System.out.println("4. Buscar productos por categoria");
+		System.out.println("5. Buscar productos por nombre");
+		System.out.println("6. Buscar productos por codigo");
+		System.out.println("7. Buscar productos por precio");
+		System.out.println("----------------------------------");
+		System.out.println("8. Calcular comision por vendedor");
+		System.out.println("----------------------------------");
+		System.out.println("9. Obtener BBDD");
+		System.out.println("----------------------------------");
+		System.out.println("10. FINALIZAR");
+		this.input = sc.nextLine();
+		limpiarConsola();
+		ejecutarOpcion(input);
+		sc.close();
 	}
 	
 
-	private static void mostrarMenu(int input) {
-		limpiarConsola();
-		switch(input) {
+	private void ejecutarOpcion(String input) {
+		Scanner  sc = new Scanner(System.in);
+		while(!esValido(input)) {
+			System.out.println("Por favor ingrese una opcion valida [1 - 10]");
+			input = sc.nextLine();
+			this.input = input;
+		}
+		sc.close();
+		
+		switch(Integer.parseInt(input)) {
 		   case 1 :
-		      new MenuProductosCLI();
-		      break;
+			   registrarProducto();
+			   break;
 		   
 		   case 2 :
-		      new MenuVendedoresCLI();
-		      break;
+			   registrarVendedor();
+			   break;
 		   
 		   case 3:
-			   new MenuVentasCLI();
+			   registrarVenta();
 			   break;
 			   
-		   case 4:
+		   case 4 :
+			   buscarProductoPorCategoria();
+			   break;
+			   
+		   case 5:
+			   buscarProductoPorNombre();
+				  break;
+				  
+		   case 6 :
+			   buscarProductoPorCodigo();
+			   break;
+					   
+		   case 7:
+			   buscarProductoPorPrecio();
+			   break;
+			   
+		   case 8:
+			   calcularComision();
+			   break;
+			   
+		   case 9:
+			   persistirBBDD();
+			   break;
+			   
+		   case 10:
 			   finalizar();
 			   break;
 		   default : 
 		     System.out.println("algo salio mal que llego al default");
 		}
-			
 		
 	}
+	
+	
 
-	private static boolean validarInput(Object input) {
+
+	private void registrarVendedor() {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingrese nombre del vendedor");
+		String inputNombre = scan.nextLine();
+		
+		System.out.println("Ingrese apellido del vendedor");
+		String inputApellido = scan.nextLine();
+		
+		System.out.println("Ingrese el sueldo del vendedor");
+		String inputSueldo = scan.nextLine();
+		
+		while(!validarDouble(inputSueldo)) {
+		    System.out.println("error, ingrese un precio valido");
+		    inputSueldo = scan.nextLine();
+		}
+		
+		Vendedor vendedor = new Vendedor();
+		vendedor.setNombre(inputNombre+" "+inputApellido);
+		vendedor.setSueldo(Double.parseDouble(inputSueldo));
+		
+		System.out.println("insertando.....");
+		
+		if(VendedorSQL.insert(vendedor)) {
+			System.out.println("INGRESO EXITOSO");
+		} else {
+			System.out.println("hubo un error al hacer el ingreso");
+		}
+		scan.close();
+
+	}
+
+
+	private static void registrarProducto() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingrese nombre del producto");
+		String inputNombre = scan.nextLine();
+
+		System.out.println("Ingrese categoria del producto");
+		String inputCategoria = scan.nextLine();
+
+		System.out.println("Ingrese el precio del producto");
+		String inputPrecio = scan.nextLine();
+		
+		while(!validarDouble(inputPrecio)) {
+		    System.out.println("error, ingrese un precio valido");
+		    System.out.println("Ingrese el precio del producto");
+		    inputPrecio = scan.nextLine();
+		}
+		
+		Producto producto = new Producto();
+		producto.setNombre(inputNombre);
+		producto.setCategoria(inputCategoria);
+		producto.setPrecio(Double.parseDouble(inputPrecio));
+		
+		System.out.println("ingresando...");
+		
+		if(ProductoSQL.insert(producto)) {
+			System.out.println("INGRESO EXITOSO");
+		} else {
+
+			System.out.println("hubo un error al ingresar el producto");
+		}
+		
+		scan.close();
+		
+	}
+	
+	private String obtenerInput() {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		sc.close();
+		return input;
+	}
+
+
+	private static boolean esValido(String input) {
 		String dato = input.toString();
-		Pattern pat = Pattern.compile("^[1-4]{1}");
+		Pattern pat = Pattern.compile("^[1-10]{1}");
 	    Matcher mat = pat.matcher(dato);                                                                           
 	    return mat.matches();
+	}
+	
+	private static boolean validarDouble(String dato) {
+		try {
+		    double precio = Double.parseDouble(dato);
+		    return true;
+		}	catch(NumberFormatException e) 
+		  {
+		    return false;
+		  }
 	}
 	
 	private static void finalizar() {
