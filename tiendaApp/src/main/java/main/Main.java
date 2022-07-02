5285785ae9049546433b2e345403d540bbfe7e4e
JoanF <fernandoene91@gmail.com>
2022-07-01 21:37:36 -0300

    falta acoplar los insert

--- tiendaApp/src/main/java/main/Main.java
+++ tiendaApp/src/main/java/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 import presentacion.MenuPrincipalCLI;
 
 public class Main 
@@ -12,33 +13,47 @@ public class Main
 		MenuPrincipalCLI cli = new MenuPrincipalCLI();
 		ArrayList<String> datos = new ArrayList<String>();
 		Scanner scan = new Scanner(System.in);
-		while(true) {
-			cli.limpiarConsola();
+		int input = 0;
+		while(input!=6) {
 			cli.getMenuPrincipal();
-			int input = scan.nextInt();
+			input = scan.nextInt();
 			while(!cli.esInputValido(input)) {
 				System.out.println("Por favor ingrese una opcion valida");
 				input = scan.nextInt();
 			}
+			
+			cli.limpiarConsola();
 			switch(input) {
 			   case 1 :
-				   cli.getDatosProducto();
+				   for(String s: cli.getDatosProducto()) {
+					   System.out.println(s);
+					   datos.add(scan.next());
+				   }
 				   break;   
 				   
 			   case 2 :
-				   cli.getDatosVendedor();
+				   for(String s : cli.getDatosVendedor()) {
+					   System.out.println(s);
+					   datos.add(scan.next());
+				   }
 				   break;
 			   
 			   case 3:
-				   cli.getDatosVenta();
+				   for(String s : cli.getDatosVenta()) {
+					   System.out.println(s);
+					   datos.add(scan.next());
+				   }
 				   break;
 				   
 			   case 4 :
 				   cli.getMenuBuscarProducto();
+				   datos.add(scan.next());
+				   System.out.println("Ingrese el dato a buscar");
+				   datos.add(scan.next());
 				   break;
 
 			   case 5:
-				   cli.calcularComision(datos);
+				   System.out.println("opcion 5");
 				   break;
 				   
 			   case 6:
@@ -47,8 +62,16 @@ public class Main
 				   
 			   default : 
 			     System.out.println("algo salio mal que llego al default");
+			    
 			}
+			
+			for(String s: datos) {
+				System.out.println(s);
+			}
+			
 		}
+		
+		scan.close();
 		
     }
 }
